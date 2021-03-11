9dd03ef18d7e80cfb482b85c3c77abb3ade997af
Corina <cborcoci@gmail.com>
2021-03-11 18:16:24 +0100

    run docs

---
+++ docs/precache-manifest.cb092ac6d7d58b9f419386f71122bd6d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4ba00b699c1b54d3139a6fc94ebf55",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f71ebd4bbb0c28e2197",
+    "url": "./static/css/main.e58ac32c.chunk.css"
+  },
+  {
+    "revision": "278ee1729681b70c8d7d",
+    "url": "./static/js/2.8f59d543.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.8f59d543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f71ebd4bbb0c28e2197",
+    "url": "./static/js/main.1f46ae06.chunk.js"
+  },
+  {
+    "revision": "5acfd12ba8d6c9ddfbe3",
+    "url": "./static/js/runtime-main.94c40845.js"
+  },
+  {
+    "revision": "070b2c4fef560aa7bd2f782cc3451081",
+    "url": "./static/media/cloudy_day.070b2c4f.svg"
+  },
+  {
+    "revision": "0a3ce6b0ccdcaeccccc1505ed29b7112",
+    "url": "./static/media/cloudy_night.0a3ce6b0.svg"
+  },
+  {
+    "revision": "fe0aefb0886fcde2ef2081826b6c2f3e",
+    "url": "./static/media/cold.fe0aefb0.svg"
+  },
+  {
+    "revision": "7e5e659bb737ceb73ecdbb4402c4488d",
+    "url": "./static/media/hot.7e5e659b.svg"
+  },
+  {
+    "revision": "7998765ed8da3ee9aca600fef5bd5900",
+    "url": "./static/media/humidity.7998765e.svg"
+  },
+  {
+    "revision": "aa8ae84da88a80cd8ae3fa951b9eabb5",
+    "url": "./static/media/meter.aa8ae84d.svg"
+  },
+  {
+    "revision": "17df37bea02d3a4878ed3027df144648",
+    "url": "./static/media/night.17df37be.svg"
+  },
+  {
+    "revision": "7e1e68aea749a60586fd378916be3956",
+    "url": "./static/media/rain.7e1e68ae.svg"
+  },
+  {
+    "revision": "8330357b77b9c519d6abd6cea84981c4",
+    "url": "./static/media/rainy_day.8330357b.svg"
+  },
+  {
+    "revision": "1396aa2df033eb58c56f74c0df0e69d7",
+    "url": "./static/media/snowflake.1396aa2d.svg"
+  },
+  {
+    "revision": "2115184cd4b94b519a240130942470cd",
+    "url": "./static/media/storm.2115184c.svg"
+  },
+  {
+    "revision": "d0e97358a08a9f163edff06e2992c668",
+    "url": "./static/media/sun.d0e97358.svg"
+  }
+]);
